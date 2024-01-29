aaa39f425b6fc76a637cb3c648a6d9ded4becbc0
Alena <krostarmotor@gmail.com>
2024-01-29 15:17:20 +0200

    1

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+	function show1() {
+		alert(1);
+	}
+	
+	function show2() {
+		alert(2);
+	}
+	
+	return <div>
+		<button>act1</button>
+		<button>act2</button>
+	</div>;
+}
+
+export default App;
